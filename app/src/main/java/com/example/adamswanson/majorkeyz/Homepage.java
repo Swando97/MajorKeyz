60b152101804d90c93f05432a1f8f6c517b121af
adamswanson <adam_swanny@hotmail.com>
2017-11-23 22:42:36 -0800

    Fix Second activity layout, add music and stop music icon!

--- app/src/main/java/com/example/adamswanson/majorkeyz/Homepage.java
+++ app/src/main/java/com/example/adamswanson/majorkeyz/Homepage.java
@@ -1,7 +1,10 @@
 package com.example.adamswanson.majorkeyz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        Button btn1 = (Button) findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.bringToFront();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Homepage.this, MainActivity.class);
+                //myIntent.putExtra("location", changeText.getText().toString());
+                startActivity(myIntent);
+            }
+        });
+
+
     }
 }
