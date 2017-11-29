5f68ad52fcf86217d389961b7f3f0fde3a8f9df6
adamswanson <adam_swanny@hotmail.com>
2017-11-28 18:09:34 -0800

    done!

--- app/src/main/java/com/example/adamswanson/majorkeyz/MainActivity.java
+++ app/src/main/java/com/example/adamswanson/majorkeyz/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.adamswanson.majorkeyz;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
